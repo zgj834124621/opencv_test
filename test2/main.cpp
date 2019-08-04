2a10d15bd1204a1d2f1e14693bbd294379915f60
zgj834124621 <834124621@qq.com>
2019-08-04 19:58:20 +0800

    second

--- test2/main.cpp
+++ test2/main.cpp
@@ -15,6 +15,22 @@ int main(int argc, char ** argv) {
 
 	namedWindow("input_image", CV_WINDOW_AUTOSIZE);
 	imshow("input_image", src);
+	dst = Mat::zeros(src.size(), src.type());
+	int cols = (src.cols-1) * src.channels();
+	int offsetx = src.channels();
+	int rows = src.rows;
+	for (int row = 1; row < (rows - 1); row++) {
+		uchar* output = dst.ptr<uchar>(row);
+		const uchar * current = src.ptr<uchar>(row);
+		const uchar * previous = src.ptr<uchar>(row-1);
+		const uchar * next = src.ptr<uchar>(row+1);
+		for (int col = offsetx; col < cols; col++) {
+			output[col] = 5 * current[col] - (current[row - offsetx] + current[col + offsetx] + previous[col] + next[col]);
+		}
+	}
+
+	namedWindow("contrast image demo", CV_WINDOW_AUTOSIZE);
+	imshow("contrast image demo", dst);
 
 	waitKey(0);
 	return 0;
